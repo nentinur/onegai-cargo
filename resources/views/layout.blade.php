9b9bd9563a4d27e5c829eb43cf0526dd1fbb0d09
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2025-08-17 12:59:44 +0700

    github page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Onegai Indonesia Cargo</title>
+    {{-- <style>
+        @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600;700&display=swap');
+    </style>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: '#D19C66',
+                        'primary-dark': '#6C5232',
+                        'primary-light': '#FBC97A',
+                    },
+                    fontFamily: {
+                        sans: ['Quicksand', 'Inter', 'sans-serif'],
+                    },
+                }
+            }
+        }
+    </script> --}}
+
     @vite('resources/css/app.css')
+
+
     <link rel="stylesheet" type="text/css"
         href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css" />
     <link rel="stylesheet" type="text/css"
@@ -13,7 +36,7 @@
 
 <body class="bg-white">
     <div class="w-full fixed top-0 left-0 h-18 z-10 flex items-center justify-between px-6 bg-white shadow-md">
-        <div><img src="{{ asset('assets/logo/OIC_BROWN (TR).png') }}" class="w-12" alt=""></div>
+        <div><img src="{{ asset('assets/logo/OIC_BROWN.png') }}" class="w-12" alt=""></div>
         <nav class="space-x-4">
             <a href="/" class="font-bold text-primary-dark hover:text-dark-primary">Beranda</a>
             <a href="/about" class="font-bold text-primary-dark hover:text-dark-primary">Tentang Kami</a>
