affb07b81b43936a3ebf87841131214dd6e74aac
zdifahk <khafidz4ever@gmail.com>
2025-09-03 17:38:15 +0700

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,4 +13,28 @@ class LoginController extends Controller
 
         ]);
     }
+
+    public function signup()
+    {
+        return view('admin.signup');
+    }
+
+    public function store(Request $request)
+    {
+        // dd($request->all());
+
+        $request->validate([
+            'name' => 'required|min:5|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
+        ]);
+
+        $data = $request->only('name', 'email', 'password');
+        $data['password'] = bcrypt($data['password']);
+        $data['created_at'] = now();
+        $data['updated_at'] = now();
+        // dd($data);
+        User::create($data);
+        return redirect('/admin')->with('success', 'Admin berhasil ditambahkan! Silakan login.');
+    }
 }
