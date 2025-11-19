98eba8f6af963b1f6da37a70915a386abcdcbacb
zdifahk <khafidz4ever@gmail.com>
2025-11-19 09:34:18 +0700

    tambah gambar carousel random

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,31 +83,41 @@
 
     {{-- Carousel Image --}}
     <div id="indicators-carousel" class="relative w-1/3" data-carousel="slide">
+        @php
+            $randomNumbers = [];
+            $jmlGambar = 5;
+            $minGambar = 1;
+            $maxGambar = 14;
+            for ($i=0; $i < $jmlGambar; $i++) { 
+                
+                $randomNumbers[] = random_int($minGambar, $maxGambar);
+            }
+        @endphp
         <!-- Carousel wrapper -->
-        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+        <div class="relative h-max overflow-hidden rounded-lg md:h-96">
             <!-- Item 1 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-                <img src="{{ asset('assets/carousel/1.png') }}" class="absolute block w-full h-full object-cover"
+                <img src="{{ asset('assets/carousel/'.$randomNumbers[0].'.png') }}" class="absolute block w-full h-full object-cover"
                     alt="...">
             </div>
             <!-- Item 2 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('assets/carousel/2.png') }}" class="absolute block w-full h-full object-cover"
+                <img src="{{ asset('assets/carousel/'.$randomNumbers[1].'.png') }}" class="absolute block w-full h-full object-cover"
                     alt="...">
             </div>
             <!-- Item 3 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('assets/carousel/3.png') }}" class="absolute block w-full h-full object-cover"
+                <img src="{{ asset('assets/carousel/'.$randomNumbers[2].'.png') }}" class="absolute block w-full h-full object-cover"
                     alt="...">
             </div>
             <!-- Item 4 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('assets/carousel/4.png') }}" class="absolute block w-full h-full object-cover"
+                <img src="{{ asset('assets/carousel/'.$randomNumbers[3].'.png') }}" class="absolute block w-full h-full object-cover"
                     alt="...">
             </div>
             <!-- Item 5 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('assets/carousel/5.png') }}" class="absolute block w-full h-full object-cover"
+                <img src="{{ asset('assets/carousel/'.$randomNumbers[4].'.png') }}" class="absolute block w-full h-full object-cover"
                     alt="...">
             </div>
         </div>
@@ -117,6 +127,12 @@
                 data-carousel-slide-to="0"></button>
             <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
                 data-carousel-slide-to="1"></button>
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
+            data-carousel-slide-to="2"></button>
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4"
+            data-carousel-slide-to="3"></button>
+            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5"
+            data-carousel-slide-to="4"></button>
         </div>
         <!-- Slider controls -->
         <button type="button"
